aee210170b88d5927f2e6d9b253686b74def5016
Yanyang <yanyang.xie@thistech.com>
2016-09-05 15:17:40 +0800

    update restart simulator script

--- src/operation/restart_simulators.sh
+++ src/operation/restart_simulators.sh
@@ -18,23 +18,24 @@ python vex_operation.py stop_cns_simulator $config_sub_tag
 python vex_operation.py start_cns_simulator $config_sub_tag
 
 #mock origin
-python vex_operation.py stop_content_router_simulator $config_sub_tag
-python vex_operation.py start_content_router_simulator $config_sub_tag
+echo "Restart origin server"
+python vex_operation.py stop_origin_server $config_sub_tag
+python vex_operation.py start_origin_server $config_sub_tag
 
 #origin proxy 
-#python vex_operation.py batch_stop_origin_proxy_simulator $config_sub_tag
-#python vex_operation.py batch_start_origin_proxy_simulator $config_sub_tag
+python vex_operation.py batch_stop_origin_proxy_simulator $config_sub_tag
+python vex_operation.py batch_start_origin_proxy_simulator $config_sub_tag
 
 #origin_simulator
-#python vex_operation.py stop_origin_simulator $config_sub_tag
-#python vex_operation.py start_origin_simulator $config_sub_tag
+python vex_operation.py stop_origin_simulator $config_sub_tag
+python vex_operation.py start_origin_simulator $config_sub_tag
 
 #cdvr simulator
-#python vex_operation.py stop_cdvr_simulator $config_sub_tag
-#python vex_operation.py start_cdvr_simulator $config_sub_tag
-#python vex_operation.py setup_cdvr_simulator_ad_insertion $config_sub_tag
+python vex_operation.py stop_cdvr_simulator $config_sub_tag
+python vex_operation.py start_cdvr_simulator $config_sub_tag
+python vex_operation.py setup_cdvr_simulator_ad_insertion $config_sub_tag
 
 #vod simulator
-#python vex_operation.py stop_vod_simulator $config_sub_tag
-#python vex_operation.py start_vod_simulator $config_sub_tag
-#python vex_operation.py setup_vod_simulator_ad_insertion $config_sub_tag
+python vex_operation.py stop_vod_simulator $config_sub_tag
+python vex_operation.py start_vod_simulator $config_sub_tag
+python vex_operation.py setup_vod_simulator_ad_insertion $config_sub_tag
