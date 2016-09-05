dd43ffafb9d1991454683fee779dd60311fd1481
root <root@ip-172-31-9-88.ap-southeast-1.compute.internal>
2016-09-05 06:51:30 +0000

    fix bug in deploy_all.sh

--- src/autodeploy/deploy_all.sh
+++ src/autodeploy/deploy_all.sh
@@ -11,7 +11,7 @@ echo "Deployment configuration sub tag is: ${deploy_config_sub_tag}"
 # All the supported compontents
 declare -a compontents=()
 compontents[1]="core_vex/deploy_core_vex.py"
-compontents[2]="vex-ui/deploy_vex_ui.py"
+compontents[2]="vex_ui/deploy_vex_ui.py"
 compontents[3]="vex_fe/deploy_vex_fe.py"
 compontents[4]="vex_director/deploy_vex_director.py"
 compontents[5]="origin_manager/deploy_vex_origin_manager.py"
@@ -35,7 +35,7 @@ do_deploy(){
        	    fi
         fi
         
-        echo "Done"
+        echo "Finish to deployment all the compontents."
     done
 }
 
