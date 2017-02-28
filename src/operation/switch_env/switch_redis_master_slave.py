bc17d6a2edb47508c4531208190586fe5ecd2831
Yanyang <yanyang.xie@thistech.com>
2017-02-28 11:49:21 +0800

    add parameter to kill redis process

--- src/operation/switch_env/switch_redis_master_slave.py
+++ src/operation/switch_env/switch_redis_master_slave.py
@@ -19,16 +19,36 @@ redis_start_cmd = 'systemctl start redis.service'
 redis_stop_cmd = 'systemctl stop redis.service'
 
 @task
-def stop_redis(redis_host):
+def stop_redis(redis_host, is_kill=True):
     with settings(host_string=redis_host):
-        print 'Stop redis server in [%s]' %(redis_host)
-        run(redis_stop_cmd)
+        if is_kill is True:
+            print 'Kill redis server in [%s]' %(redis_host)
+            kill_redis_service()
+        else:
+            print 'Stop redis server in [%s]' %(redis_host)
+            run(redis_stop_cmd)
 
 @task
 def start_redis(redis_host):
     with settings(host_string=redis_host):
         print 'Start redis server in [%s]' %(redis_host)
         run(redis_start_cmd)
+
+def kill_redis_service(service_tag='redis-server'):
+    pid = run("ps gaux | grep %s | grep -v grep | awk '{print $2}'" % (service_tag), pty=False, warn_only=True)
+    if pid == '':
+        return
+
+    pids = str(pid).splitlines()
+    if pids is None or len(pids) == 0:
+        print 'Service \'%s\' is not running till now.' % (service_tag)
+    else:
+        print 'Service \'%s\' is running, execute kill.' % (service_tag)
+        for p_id in pids:
+            try:
+                run('kill -9 %s' % (p_id), pty=False, warn_only=True, quiet=True)
+            except:
+                pass
 
 if __name__ == '__main__':
     if len(redis_hosts) != 2:
@@ -38,18 +58,19 @@ if __name__ == '__main__':
     env.user=user
     env.key_filename=public_key_file
     env.port=port
+    is_kill=True
     
-    start_stop_interval = 30
+    start_stop_interval = 300
     
     counter = 0
     while(True):
         redis_host_1, redis_host_2 = redis_hosts
-        execute(stop_redis, redis_host_1)
+        execute(stop_redis, redis_host_1, is_kill)
         sleep(start_stop_interval)
         execute(start_redis, redis_host_1)
         sleep(start_stop_interval)
         
-        execute(stop_redis, redis_host_2)
+        execute(stop_redis, redis_host_2, is_kill)
         sleep(start_stop_interval)
         execute(start_redis, redis_host_2)
         sleep(start_stop_interval)
